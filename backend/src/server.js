d47402afeace0b4f49fe4835131f0f723a133d53
Adithya Manthena <adithyamanthena@gmail.com>
2025-07-29 21:26:38 +0530

    Prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,11 @@ import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
+
+
 const app=express();
+const __dirname=path.resolve();
 console.log(" MONGO_URI:", process.env.MONGO_URI);
 connectDB();
 
@@ -22,6 +26,14 @@ app.use(cors({
 //app.use(rateLimiter);
 app.use("/api/notes",notesRoutes);
 
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // app.get("api/notes",(req,res)=>{
 //     res.status(200).send({message:"Note created successfully!"});
